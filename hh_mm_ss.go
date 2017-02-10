07067d2314eb9fba894d7f0bd5a5db34ed12952e
aomnes <antoine.omnes@utbm.fr>
2017-02-10 15:21:37 +0100

    mise en forme

--- hh_mm_ss.go
+++ hh_mm_ss.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-    for true {
-    	t := time.Now()
-    	fmt.Printf("%d:", t.Hour())
-    	fmt.Printf("%d:", t.Minute())
-    	fmt.Printf("%d\n", t.Second())
-    }
+	for true {
+		t := time.Now()
+		fmt.Printf("%d:", t.Hour())
+		fmt.Printf("%d:", t.Minute())
+		fmt.Printf("%d\n", t.Second())
+	}
 }
