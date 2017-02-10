69b78b59421016fb63dc08b67f1abcd5017b1a1e
aomnes <antoine.omnes@utbm.fr>
2017-02-10 16:13:28 +0100

    ajout 0

--- hh_mm_ss.go
+++ hh_mm_ss.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 	"gofmt"
+	"os"
+	"os/exec"
 	"time"
 )
 
 func main() {
 	for true {
+		clear := exec.Command("clear")
+		clear.Stdout = os.Stdout
+		clear.Run()
 		t := time.Now()
+		if t.Hour() <= 9 {
+			fmt.Printf("0")
+		}
 		fmt.Printf("%d:", t.Hour())
+		if t.Minute() <= 9 {
+			fmt.Printf("0")
+		}
 		fmt.Printf("%d:", t.Minute())
+		if t.Second() <= 9 {
+			fmt.Printf("0")
+		}
 		fmt.Printf("%d\n", t.Second())
 	}
 }
